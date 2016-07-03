5ce1834e4f6d462a2d0eea6fc32c84f9e57a568d
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-07-02 22:29:18 -0400

    Shuffle deck
    update scripts to shuffle the deck

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -16,6 +16,9 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+		Deck.Shuffle(ref deck.cards); // This shuffles the deck
+		// The ref keyword passes a reference to deck.cards, which allows
+		// deck.cards to be modified by Deck.Shuffle()
 	}
 
 }
