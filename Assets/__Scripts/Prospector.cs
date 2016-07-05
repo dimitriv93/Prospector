e25558d66f3105c9a4a75eb56f6d35a811ae2255
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-07-05 14:18:00 -0400

    Game over
    Script for checking if the game is over

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -126,6 +126,8 @@ public class Prospector : MonoBehaviour {
 			SetTableauFaces(); // Update tableau card face-ups
 			break;
 		}
+		// Check to see whether the game is over or not
+		CheckForGameOver();
 	}
 
 	// This turns cards in the Mine face-up or face-down
@@ -222,6 +224,40 @@ public class Prospector : MonoBehaviour {
 		if (c0.rank == 13 && c1.rank == 1) return(true);
 		// Otherwise, return false
 		return(false);
+	}
+
+	// Test whether the game is over
+	void CheckForGameOver() {
+		// If the tableau is empty, the game is over
+		if (tableau.Count==0) {
+			// Call GameOver() with a win
+			GameOver(true);
+			return;
+		}
+		// If there are still cards in the draw pile, the game's not over
+		if (drawPile.Count>0) {
+			return;
+		}
+		// Check for remaining valid plays
+		foreach ( CardProspector cd in tableau ) {
+			if (AdjacentRank(cd, target)) {
+				// If there is a valid play, the game's not over
+				return;
+			}
+		}
+		// Since there are no valid plays, the game is over
+		// Call GameOver with a loss
+		GameOver (false);
+	}
+	// Called when the game is over. Simple for now, but expandable
+	void GameOver(bool won) {
+		if (won) {
+			print ("Game Over. You won! :)");
+		} else {
+			print ("Game Over. You Lost. :(");
+		}
+		// Reload the scene, resetting the game
+		Application.LoadLevel("__Prospector_Scene_0");
 	}
 
 
