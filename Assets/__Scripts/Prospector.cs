649f4d29f493aceab34d0a9f81ee43faf26cc6dd
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-07-05 12:05:21 -0400

    Laying Out the Mine Tableau
    Changing cars to card prospectors

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -8,6 +8,9 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 	public Deck					deck;
 	public TextAsset			deckXML;
+	public Layout layout;
+	public TextAsset layoutXML;
+	public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this;
@@ -19,6 +22,18 @@ public class Prospector : MonoBehaviour {
 		Deck.Shuffle(ref deck.cards); // This shuffles the deck
 		// The ref keyword passes a reference to deck.cards, which allows
 		// deck.cards to be modified by Deck.Shuffle()
+		layout = GetComponent<Layout>(); // Get the Layout
+		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+		drawPile = ConvertListCardsToListCardProspectors( deck.cards );
+	}
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach( Card tCD in lCD ) {
+			tCP = tCD as CardProspector; // 1
+			lCP.Add( tCP );
+		}
+		return( lCP );
 	}
 
 }
